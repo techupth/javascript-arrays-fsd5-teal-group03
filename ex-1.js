b41a99793c7654cb8845581a4af5bce2e09aa866
Panupong <Pai28212m@gmail.com>
2023-11-28 14:08:08 +0700

    HH:Pai2

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,10 @@
 let colors = ["red", "green", "blue", "khaki", "orange"];
 
 // Start coding here
-let blueColor = colors[0];
+let blueColor = colors[2];
 let fourthColor = colors[3];
 let fifthColor = colors[4];
-let tenthColor = colors[10];
+let tenthColor = colors[9];
 
 console.log(
   blueColor + " " + fourthColor + " " + fifthColor + " " + tenthColor
